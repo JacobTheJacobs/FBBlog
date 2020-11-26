26408efece71c9583cae9a3bce0ea0c81449a903
jakeei212 <jake.yanov@gmail.com>
2020-11-26 20:24:05 +0200

    carousel

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,22 +1,38 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux'
-import {fetchPosts} from '../../store/actions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../../store/actions";
+import {Link} from 'react-router-dom'
+import CarouselWidget from '../../utils/carousel'
 class Home extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts(6, null));
+  }
 
-    componentDidMount(){
-
-this.props.dispatch(fetchPosts(6,null));
-    }
-    render() {
-        return (
-            <div>
-                
+  renderFeaturePosts = () =>
+    this.props.reviews.posts
+      ? this.props.reviews.posts.map((item, i) => (
+          <div className="col ">
+            <div  key={i} className="card" style={{width: '18rem'}}>
+             
+              <div className="card-body">
+                  <h6 className="alert alert-primary">{item.title}</h6>
+                <p className="card-text">{item.excerpt}</p>
+              <Link to={`/reviews/${item.id}`}>Read More</Link>
+              </div>
             </div>
-        )
-    }
+          </div>
+        ))
+      : null;
+
+  render() {
+    return <div className="container ">
+        <CarouselWidget/>
+        <div className="row">   {this.renderFeaturePosts()}</div>
+     </div>;
+  }
 }
-const mapStateToProps = state => ({
-    reviews:state.reviews
-})
+const mapStateToProps = (state) => ({
+  reviews: state.reviews,
+});
 
 export default connect(mapStateToProps)(Home);
