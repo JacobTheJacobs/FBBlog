99c87d9062e9d45eed7046554f6b8ed93c18b305
Master J <jake.yanov@gmail.com>
2020-12-02 15:13:49 +0200

    almost

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,51 +3,84 @@ import { connect } from "react-redux";
 import { fetchPosts } from "../../store/actions";
 import { Link } from "react-router-dom";
 import CarouselWidget from "../../utils/carousel";
-import { Container, Row, Col,Card } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 class Home extends Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts(6, null));
   }
 
-  
   renderFeaturePosts = () =>
     this.props.reviews.posts
-    
       ? this.props.reviews.posts.map((item, i) => (
-          <Col xs={12} sm={4} md={3} lg={2} key={i} className="card col" style={{ width: "38rem",margin:"10px" }}>
-           <div>
-            <div className="card-body">
-              <div style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center"
-              }}>
-<h4 style={{height:'100px'}} className="alert alert-primary"><strong>{item.title}</strong></h4>
-              </div>
-              
-            
+          <Col
+            xs={12}
+            sm={4}
+            md={3}
+            lg={2}
+            key={i}
+            className="card col"
+            style={{ margin: "10px", padding: "0px" }}
+          >
+            <div>
+              <div className="card-body">
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <div className="alert alert-primary">
+                    <h4 style={{ height: "80px" }}>
+                      <strong>{item.title}</strong>
+                    </h4>
+                  </div>
+                </div>
 
-              <p style={{height:'70px'}} className="card-text blockquote text-center">{item.excerpt}</p>
-              <Link className="blockquote text-right blockquote-footer" to={`/reviews/${item.id}`}>Read More</Link>
+                <p
+                  style={{ height: "70px" }}
+                  className="card-text blockquote text-center"
+                >
+                  {item.excerpt}
+                </p>
+                <Link
+                  className="btn btn-outline-danger btn-lg btn-block"
+                  to={`/reviews/${item.id}`}
+                >
+                  Read More
+                </Link>
+              </div>
             </div>
-           </div>
-          </Col >
+          </Col>
         ))
       : null;
 
   render() {
     return (
       <>
-      <div>
-      <CarouselWidget />
-      </div>
-       
-      <Container>
-          <Row style={{justifyContent:'center'}}>
-           {this.renderFeaturePosts()}
-          </Row>
-      </Container>
+        <br></br>
+        <div>
+          <CarouselWidget />
+        </div>
+        <br></br>
+
+        <Container>
+          <div
+            className="card"
+            style={{
+              backgroundImage:
+                'url("https://www.citationmachine.net/wp-content/uploads/2019/07/Top-5-Tips-for-a-Successful-Study-Session-CitationMachine-blog-e1522941843154.jpg")',
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+          >
+            <Row style={{ justifyContent: "center" }}>
+              {this.renderFeaturePosts()}
+            </Row>
+          </div>
+        </Container>
       </>
     );
   }
--- src/utils/carousel.js
+++ src/utils/carousel.js
@@ -1,38 +1,48 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel'
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
 
 const CarouselWidget = () => {
-    return (
-        <Carousel>
-            <Carousel.Item>
-                <img className="d-click w-100"
-               alt="pic"
-               style={{
-                   width:'300px',
-                   height:'300px'
-               }}
-               src="https://www.gqrgm.com/wp-content/uploads/2019/09/2.-Blog-Banner-Template.jpg"/>
-           <Carousel.Caption>
-               <h3>Motivation</h3>
-               <p>Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.</p>
-           </Carousel.Caption>
-            </Carousel.Item>
-           
-            <Carousel.Item>
-                <img className="d-click w-100"
-               alt="pic"
-               style={{
-                   width:'300px',
-                   height:'300px'
-               }}
-               src="https://shlomi.ort.org.il/wp-content/uploads/2018/10/math.jpg"/>
-                       <Carousel.Caption>
-               <h3>Keep Going</h3>
-               <p>I quickly came to understand that code is a superpower every young woman should be able to access. Understanding that code is the underlying (and invisible) framework of tech means that we do not have to be passive bystanders in our ever-changing digital world.</p>
-           </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
-    );
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-click w-100"
+          alt="pic"
+          style={{
+            width: "300px",
+            height: "300px",
+          }}
+          src="https://www.publicdomainpictures.net/pictures/210000/velka/yellow-wood-texture-wallpaper.jpg"
+        />
+        <Carousel.Caption>
+          <h3>Motivation</h3>
+          <p>
+            Any fool can write code that a computer can understand. Good
+            programmers write code that humans can understand.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-click w-100"
+          alt="pic"
+          style={{
+            width: "300px",
+            height: "300px",
+          }}
+          src="https://i.pinimg.com/originals/66/83/35/668335b6676cabfdc23ad9c9428c400a.jpg"
+        />
+        <Carousel.Caption>
+          <h3>Keep Going</h3>
+          <p>
+            First learn computer science and all the theory. Next develop a
+            programming style. Then forget all that and just hack
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 };
 
 export default CarouselWidget;
