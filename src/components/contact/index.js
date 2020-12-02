a29af2038587874e7b05b45ae57b6d0d4be4db0d
Master J <jake.yanov@gmail.com>
2020-12-02 15:17:03 +0200

    almost..

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -6,77 +6,6 @@ const Contact = () => {
     <div className="container">
       <div className="main-body">
         <div className="row gutters-sm">
-          <div className="col-md-4 mb-3">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex flex-column align-items-center text-center">
-                  <img
-                    src="https://www.picclickimg.com/d/l400/pict/282657064087_/Simpsons-Portrait-Yellow-Character-Custom-Made-From-Your.jpg"
-                    alt="Admin"
-                    className="rounded-circle"
-                    style={{ width: "100px" }}
-                  />
-                  <div className="mt-3">
-                    <h4>Jacob Yanov</h4>
-                    <p className="text-secondary mb-1">Full Stack Developer</p>
-                    <p className="text-muted font-size-sm">Haifa, Israel</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="card mt-3">
-              <ul className="list-group list-group-flush">
-                <a
-                  href="https://github.com/jakeei212"
-                  className="list-group-item d-flex justify-content-between align-items-center flex-wrap"
-                >
-                  <h6 className="mb-0">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      className="feather feather-github mr-2 icon-inline"
-                    >
-                      <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                    </svg>
-                  </h6>
-                  <span className="text-secondary">Github</span>
-                </a>
-
-                <a
-                  href="https://www.linkedin.com/in/jacob-yanov/"
-                  className="list-group-item d-flex justify-content-between align-items-center flex-wrap"
-                >
-                  <h6 className="mb-0">
-                    {" "}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      className="feather feather-linkedin  mr-2 icon-inline text-info"
-                    >
-                      <circle cx="4" cy="5" r="2"></circle>
-                      <rect x="2" y="10.25" width="4" height="11.75"></rect>
-                      <path d="M17.3837 10c-1.8723 0-3.1275.9705-3.6414 1.891h-.0515v-1.6H10V22h3.8448v-5.7923c0-1.527.3074-3.0062 2.3097-3.0062 1.9735 0 1.9998 1.7467 1.9998 3.1043V22H22v-6.4224C22 12.4252 21.2806 10 17.3837 10" />
-                    </svg>
-                  </h6>
-                  <span className="text-secondary">LinkedIn</span>
-                </a>
-              </ul>
-            </div>
-          </div>
           <div className="col-md-8">
             <div className="card mb-3">
               <div className="card-body">
@@ -238,6 +167,77 @@ const Contact = () => {
                   </div>
                 </div>
               </div>*/}
+            </div>
+          </div>
+          <div className="col-md-4 mb-3">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex flex-column align-items-center text-center">
+                  <img
+                    src="https://www.picclickimg.com/d/l400/pict/282657064087_/Simpsons-Portrait-Yellow-Character-Custom-Made-From-Your.jpg"
+                    alt="Admin"
+                    className="rounded-circle"
+                    style={{ width: "100px" }}
+                  />
+                  <div className="mt-3">
+                    <h4>Jacob Yanov</h4>
+                    <p className="text-secondary mb-1">Full Stack Developer</p>
+                    <p className="text-muted font-size-sm">Haifa, Israel</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card mt-3">
+              <ul className="list-group list-group-flush">
+                <a
+                  href="https://github.com/jakeei212"
+                  className="list-group-item d-flex justify-content-between align-items-center flex-wrap"
+                >
+                  <h6 className="mb-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      className="feather feather-github mr-2 icon-inline"
+                    >
+                      <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                    </svg>
+                  </h6>
+                  <span className="text-secondary">Github</span>
+                </a>
+
+                <a
+                  href="https://www.linkedin.com/in/jacob-yanov/"
+                  className="list-group-item d-flex justify-content-between align-items-center flex-wrap"
+                >
+                  <h6 className="mb-0">
+                    {" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      className="feather feather-linkedin  mr-2 icon-inline text-info"
+                    >
+                      <circle cx="4" cy="5" r="2"></circle>
+                      <rect x="2" y="10.25" width="4" height="11.75"></rect>
+                      <path d="M17.3837 10c-1.8723 0-3.1275.9705-3.6414 1.891h-.0515v-1.6H10V22h3.8448v-5.7923c0-1.527.3074-3.0062 2.3097-3.0062 1.9735 0 1.9998 1.7467 1.9998 3.1043V22H22v-6.4224C22 12.4252 21.2806 10 17.3837 10" />
+                    </svg>
+                  </h6>
+                  <span className="text-secondary">LinkedIn</span>
+                </a>
+              </ul>
             </div>
           </div>
         </div>
